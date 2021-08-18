75c0cc2a9b49c4d77d406daa34951fe49af614de
JMTE <JMTEdeveloper@outlook.es>
2021-08-18 16:46:17 +0200

    Nos valemos de un template publico para hacer diferentes pruebas con express

--- app.js
+++ app.js
@@ -8,13 +8,23 @@ app.use(express.static('public'))
  
 
  
-app.get('/hola-mundo', function (req, res) {
-    res.send('Hola Mundo en su ruta')
+// app.get('/hola-mundo', function (req, res) {
+//     res.send('Hola Mundo en su ruta')
+//   })
+
+//   app.get('*', function (req, res) {
+//     res.sendFile(__dirname+'/public/404.html')  //El dirname nos construye el path de donde se encuentra la app
+//   })
+
+  app.get('/generic', function (req, res) {
+    res.sendFile(__dirname+'/public/generic.html')
   })
 
-  app.get('*', function (req, res) {
-    res.sendFile(__dirname+'/public/404.html')  //El dirname nos construye el path de donde se encuentra la app
+  app.get('/elements', function (req, res) {
+    res.sendFile(__dirname+'/public/elements.html')
   })
+
+
 app.listen(port, ()=>{
     console.log(`El ejemplo que hemos realizado esta corriendo en el puerto ${port}`);
 });
