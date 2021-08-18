c821a990c0f708848ca22db3346db4a4d3d15957
JMTE <JMTEdeveloper@outlook.es>
2021-08-18 17:29:55 +0200

    Se utilizan parciales para aprovechar codigo en todas las paginas (header,navbar,footer) etcetc , todo con handlebar

--- app.js
+++ app.js
@@ -1,11 +1,16 @@
-const express = require('express')
+const express = require('express');
+const hbs = require('hbs');
 const app = express()
 const port=8080;
 
 
+//Handlebars
+app.set('view engine', 'hbs');
+
+hbs.registerPartials(__dirname + '/views/parciales');
 //Crear carpeta views
 
-app.set('view engine', 'hbs');
+
 
 //Servir contenido estatico
 
@@ -20,22 +25,25 @@ app.use(express.static('public'))
 //   app.get('*', function (req, res) {
 //     res.sendFile(__dirname+'/public/404.html')  //El dirname nos construye el path de donde se encuentra la app
 //   })
+
+const datos={
+
+  nombre:'JMTE',
+  titulo:'NodeJS'
+
+}
    
 app.get('/', function (req, res) {
-  res.render('home',{
-
-      nombre:'JMTE',
-      titulo:'NodeJS'
-  });
+  res.render('home',datos);
 })
 
 
   app.get('/generic', function (req, res) {
-    res.sendFile(__dirname+'/public/generic.html')
+    res.render('generic',datos)
   })
 
   app.get('/elements', function (req, res) {
-    res.sendFile(__dirname+'/public/elements.html')
+    res.render('elements',datos)
   })
 
 
